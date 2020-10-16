3f4daed3651d9927ba5c9ead6041e8da5fab49a3
AStepanyan21 <51211218+AStepanyan21@users.noreply.github.com>
2020-10-16 21:04:59 +0400

    end homework

---
+++ Multiples/Multiples/Multiples.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int sum(int number) {
+    int count = 0;
+    for (int i = 0; i < number; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            count += i;
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    int number;
+    std::cin >> number;
+    std::cout << sum(number) << std::endl;
+    return 0;
+}
---
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int longestSubstringWithoutRepeatingCharacters(string str)
+{
+    int n = str.size();
+    int res = 0;  
+
+    for (int i = 0; i < n; i++) {
+        vector<bool> visited(256);
+
+        for (int j = i; j < n; j++) {
+            if (visited[str[j]] == true)
+                break; 
+            else {
+                res = max(res, j - i + 1);
+                visited[str[j]] = true;
+            }
+        }
+        visited[str[i]] = false;
+    }
+    return res;
+}
+
+int main()
+{
+    string str ;
+    cin >> str ;
+    cout << "The input string is " << str << endl;
+    int len = longestSubstringWithoutRepeatingCharacters(str);
+    cout << " Length of substring without repetitions -" <<len;
+    return 0;
+}
