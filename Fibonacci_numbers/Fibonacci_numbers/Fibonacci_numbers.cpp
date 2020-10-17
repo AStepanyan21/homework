dc363abadef5fc16a52a8fa0cdc2c43c72fd0e0c
AStepanyan21 <51211218+AStepanyan21@users.noreply.github.com>
2020-10-17 19:15:15 +0400

    Even Fibonacci numbers

---
+++ Fibonacci_numbers/Fibonacci_numbers/Fibonacci_numbers.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int count = 0;
+int even_valued = 0;
+
+void fibonachi_numbers(int num1, int num2) {
+    
+    int num3;
+    num3 = num1 + num2;
+    if (num3 <= 4000000) {
+        if (num3 % 2 == 0) {
+            even_valued += num3;
+        }
+        count += 1;
+        return fibonachi_numbers(num2, num3);
+    }
+    else {
+        std::cout << even_valued;
+    }
+}
+
+int main()
+{
+     fibonachi_numbers(0,1);
+}
