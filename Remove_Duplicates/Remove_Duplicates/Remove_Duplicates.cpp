14aea8fd0d4ae5df64ba66306e3a145dd2bc0ee8
AStepanyan21 <51211218+AStepanyan21@users.noreply.github.com>
2020-10-17 20:20:49 +0400

    Remove Duplicates

---
+++ Remove_Duplicates/Remove_Duplicates/Remove_Duplicates.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+
+ int removeDuplicates(std::vector<int>& nums,int n) {
+        if (n == 0 || n == 1)
+            return n;
+        std::vector<int> temp;
+        temp.resize(n);
+        int j = 0;
+        for (int i = 0; i < n - 1; i++)
+            if (nums[i] != nums[i + 1])
+                temp[j++] = nums[i];
+        temp[j++] = nums[n - 1];
+        for (int i = 0; i < j; i++)
+            nums[i] = temp[i];
+        return j;
+};
+
+int main()
+{
+    std::vector<int> arr{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+    int n = arr.size();
+    std::cout<< removeDuplicates(arr, n);
+    return 0;
+}
