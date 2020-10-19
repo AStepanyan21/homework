c3588de8edcf9c217bc26ef649414b8b455c1976
AStepanyan21 <51211218+AStepanyan21@users.noreply.github.com>
2020-10-19 23:02:57 +0400

    Largest prime factor

---
+++ Largest_prime_factor/Largest_prime_factor/Largest_prime_factor.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <math.h>
+
+long long  maxPrimeFactors(long long num) {
+    long long maxPrime = -1;
+    while (num % 2 == 0) {
+        maxPrime = 2;
+        num /= 2;
+    }
+
+    for (int i = 3; i < sqrt(num); i+=2) {
+        while (num % i == 0) {
+            maxPrime = i;
+            num = num / i;
+        }
+    }
+    if (num > 2) {
+        maxPrime = num;
+    }
+    return maxPrime;
+}
+
+int main()
+{
+    long long numer = 600851475143;
+    std::cout << maxPrimeFactors(numer);
+}
