81e64557ef1e4ec18331f92b8ccbfb7b577a7bde
AStepanyan21 <51211218+AStepanyan21@users.noreply.github.com>
2020-10-20 20:28:11 +0400

    Integer to Roman

---
+++ Integer_to_Roman/Integer_to_Roman/Integer_to_Roman.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+void intToRoman(int num) {
+    std::string str_romans[] = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+    int values[] = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+
+    std::string result = "";
+
+    for (int i = 0; i < 13; ++i)
+    {
+        while (num - values[i] >= 0)
+        {
+            result += str_romans[i];
+            num -= values[i];
+        }
+    }
+
+    std::cout << result;
+}
+
+int main()
+{
+    int number = 42;
+    intToRoman(number);
+}
