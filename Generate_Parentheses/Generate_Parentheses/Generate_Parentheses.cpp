d0a68a9e3d9d992d139b4312c81f7cecfd4cda09
AStepanyan21 <51211218+AStepanyan21@users.noreply.github.com>
2020-10-28 19:20:59 +0400

    Generate Parentheses

---
+++ Generate_Parentheses/Generate_Parentheses/Generate_Parentheses.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+
+void printParenthesis(int pos, int n, int open, int close)
+{
+    static char str[8];
+    if (n > 0) {
+        if (close == n)
+        {
+            std::cout << str << std::endl;
+
+        }
+        else
+        {
+            if (open > close)
+            {
+                str[pos] = ')';
+                printParenthesis(pos + 1, n, open, close + 1);
+            }
+
+            if (open < n)
+            {
+                str[pos] = '(';
+                printParenthesis(pos + 1, n, open + 1, close);
+            }
+        }
+    }
+}
+
+
+
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    printParenthesis(0,n,0,0);
+}
