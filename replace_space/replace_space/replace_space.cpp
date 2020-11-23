982a8ee0caf7f4f83b863248aadad39fb689ef6d
AStepanyan21 <51211218+AStepanyan21@users.noreply.github.com>
2020-11-23 20:28:49 +0400

    replace space

---
+++ replace_space/replace_space/replace_space.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+int lengths(char* str) {
+    int index = 0;
+    while (str[index] != '\0') {
+        index++;
+    }
+    return index;
+}
+
+void replace_space(char* str, int space_count) {
+    int buff = space_count * 2;
+    int len = lengths(str);
+    for (int i = 0; i < len - buff; i++) {
+        if (str[i] == ' ') {
+            for (int j = len - 1; j > i; j--) {
+                str[j] = str[j - 2];
+            }
+            str[i] = '%';
+            str[i + 1] = '2';
+            str[i + 2] = '0';
+        }
+    }
+    std::cout << str;
+}
+
+
+int main()
+{
+    char str[] = "Hello World  ";
+    std::cout << str << std::endl;
+    replace_space(str, 1);
+}
